3530e397606dd799ae39aac2f2bfe342e5165560
arehmanllc <checkmate11926@gmail.com>
2026-04-14 14:38:17 +0500

    first

---
+++ assets/pubsub.js
@@ -0,0 +1,2 @@
+let subscribers={};function subscribe(eventName,callback){return subscribers[eventName]===void 0&&(subscribers[eventName]=[]),subscribers[eventName]=[...subscribers[eventName],callback],function(){subscribers[eventName]=subscribers[eventName].filter(cb=>cb!==callback)}}function publish(eventName,data){subscribers[eventName]&&subscribers[eventName].forEach(callback=>{callback(data)})}
+//# sourceMappingURL=/s/files/1/0666/3752/9246/t/2/assets/pubsub.js.map?v=1730755925
